5a263896cb169e77509a746ddb58f87b057e9dc0
vivek <vickfreeindia@gmail.com>
2021-09-09 19:17:25 +0530

    digitalresume_initial

--- JS/script.js
+++ JS/script.js
@@ -35,7 +35,7 @@ function frontFunction() {
 function backFunction() {
     document.getElementById("box_1").style.display="block";
     document.getElementById("box_2").style.display="none";
-    document.getElementById("box_3").style.display="block";
+    document.getElementById("box_3").style.display="none";
     document.getElementById("box_4").style.display="block";
     document.getElementById("box_5").style.display="none";
     document.getElementById("box_6").style.display="none";
@@ -44,7 +44,7 @@ function backFunction() {
     document.getElementById("box_9").style.display="none";
     document.getElementById("box_10").style.display="none";
     document.getElementById("box_11").style.display="none";
-    document.getElementById("box_12").style.display="none";
+    document.getElementById("box_12").style.display="block";
 }
 function scvFunction() {
     document.getElementById("box_1").style.display="none";
@@ -60,6 +60,6 @@ function scvFunction() {
     document.getElementById("box_9").style.display="block";
     document.getElementById("box_10").style.display="none";
     document.getElementById("box_11").style.display="none";
-    document.getElementById("box_12").style.display="block";
+    document.getElementById("box_12").style.display="none";
 
 }
